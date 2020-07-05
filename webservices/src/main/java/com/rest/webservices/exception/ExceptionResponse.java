ca5342939b9b5ac3205811932578658c44ee2c99
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-07-05 19:58:54 +0530

    Implementing Generic Exception Handling For All Resources

---
+++ webservices/src/main/java/com/rest/webservices/exception/ExceptionResponse.java
@@ -0,0 +1,15 @@
+package com.rest.webservices.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+@AllArgsConstructor
+public class ExceptionResponse{
+    private Date timeStamp;
+    private String message;
+    private String details;
+}
