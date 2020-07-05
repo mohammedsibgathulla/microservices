9b9efbc6162fc883719af14f2673d4d044238343
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-07-05 21:53:05 +0530

    Implementing HATEOAS for RESTful Services

--- webservices/src/main/java/com/rest/webservices/user/User.java
+++ webservices/src/main/java/com/rest/webservices/user/User.java
@@ -16,4 +16,6 @@ public class User {
     private String name;
     @Past
     private Date birthDate;
+
+
 }
--- webservices/src/main/java/com/rest/webservices/user/UserResource.java
+++ webservices/src/main/java/com/rest/webservices/user/UserResource.java
@@ -1,6 +1,8 @@
 package com.rest.webservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -8,6 +10,11 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 public class UserResource {
@@ -20,11 +27,18 @@ public class UserResource {
     }
 
     @GetMapping("users/{id}")
-    public User retrieveUser(@PathVariable int id) {
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
         User user = userDaoService.findOne(id);
         if(user == null)
             throw new UserNotFoundException("id-"+id);
-        return user;
+
+
+        //HATEOAS
+        EntityModel<User> resource = EntityModel.of(user);
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+
+        resource.add(linkTo.withRel("all-users"));
+        return resource;
     }
     //Input - Details of User
     //Output - CREATED & Return the created URI
