74e2e34764cef9c0b1f34d52652624cc42eff1f9
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-07-06 16:53:05 +0530

    Internationalization For RESTful Services

--- webservices/src/main/java/com/rest/webservices/WebservicesApplication.java
+++ webservices/src/main/java/com/rest/webservices/WebservicesApplication.java
@@ -1,7 +1,15 @@
 package com.rest.webservices;
 
+import org.hibernate.Session;
+import org.hibernate.validator.messageinterpolation.ResourceBundleMessageInterpolator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class WebservicesApplication {
@@ -10,4 +18,17 @@ public class WebservicesApplication {
 		SpringApplication.run(WebservicesApplication.class, args);
 	}
 
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+		resourceBundleMessageSource.setBasename("messages");
+		return  resourceBundleMessageSource;
+	}
 }
--- webservices/src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
+++ webservices/src/main/java/com/rest/webservices/helloworld/HelloWorldController.java
@@ -1,9 +1,15 @@
 package com.rest.webservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String helloWorld() {
         return "Hello World";
@@ -22,5 +28,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldWithPathVariable(@PathVariable  String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 }
