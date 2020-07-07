d5966d4c567465b39eb5942d1b31894366ed87d2
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-07-07 21:52:43 +0530

    Creating User Entity And Some Test Data

--- webservices/src/main/java/com/rest/webservices/user/User.java
+++ webservices/src/main/java/com/rest/webservices/user/User.java
@@ -4,13 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+@Entity
 @Getter
 @Setter
 @AllArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2, message = "Name should have atleast 2 characters")
     private String name;
