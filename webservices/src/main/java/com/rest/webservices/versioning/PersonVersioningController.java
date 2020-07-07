b58dd6e33c9c2375f6e0ac29503d6d161124ea04
mohammedsibgathulla <mohammed.shaik81@gmail.com>
2020-07-07 20:44:36 +0530

    Versioning RESTful Services - Header And Content Negotiation Approach

--- webservices/src/main/java/com/rest/webservices/versioning/PersonVersioningController.java
+++ webservices/src/main/java/com/rest/webservices/versioning/PersonVersioningController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonVersioningController {
+    //Here we are doing versioning using uri versioning
     @GetMapping("v1/person")
     public Person1 personV1() {
         return  new Person1("Bob Charlie");
@@ -13,4 +14,35 @@ public class PersonVersioningController {
     public Person2 personV2() {
         return  new Person2(new Name("Bob", "Charlie"));
     }
+    //Here we are doing versioning using RequestParameters
+    @GetMapping(value="/person/param", params = "version=1")
+    public Person1 paramV1() {
+        return  new Person1("Bob Charlie");
+    }
+    @GetMapping(value="/person/param", params = "version=2")
+    public Person2 paramV2() {
+        return  new Person2(new Name("Bob", "Charlie"));
+    }
+
+    //Here we are doing versioning using Header Versioning
+    @GetMapping(value="/person/header", headers = "X-API-VERSION=1")
+    public Person1 headerV1() {
+        return  new Person1("Bob Charlie");
+    }
+    @GetMapping(value="/person/header", headers = "X-API-VERSION=2")
+    public Person2 headerV2() {
+        return new Person2(new Name("Bob", "Charlie"));
+    }
+
+    //Here we are doing versioning using produces also called Content Negotiation or Accept Versioning
+    @GetMapping(value="/person/produces", produces = "application/vnd.company.app-v1+json")
+    public Person1 producesV1() {
+        return  new Person1("Bob Charlie");
+    }
+    @GetMapping(value="/person/produces", produces = "application/vnd.company.app-v2+json")
+    public Person2 producesV2() {
+        return new Person2(new Name("Bob", "Charlie"));
+    }
+
+
 }
